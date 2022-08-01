8bb2021b86dfde2f262b988e2a10e14eaa28bd4a
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-08-01 18:42:37 -0300

    Agregar h1 a #content

--- src/inimodule.js
+++ src/inimodule.js
@@ -1,5 +1,8 @@
 function iniciar () {
-    console.log(" soy funcion iniciar");
+    const content = document.querySelector("#content");
+    const heading = document.createElement("h1");
+    heading.textContent = "Restaurant Salvaje La Venancia";
+    content.appendChild(heading);
 };
 
 export default iniciar;
