95dd8625603f9ff4824049c0ef6d3c770fa0ad85
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-08-02 14:29:02 -0300

    Agregar mejor estilo al heading

--- src/inimodule.js
+++ src/inimodule.js
@@ -1,7 +1,7 @@
 function iniciar () {
     const content = document.querySelector("#content");
     const heading = document.createElement("h1");
-    heading.innerHTML = '<div class="res">Restaurant</div><div class="sal">Salvaje</div><div class="ven">“La Venancia”</div>';
+    heading.innerHTML = '<div class="izquierda"><div class="res">Restaurant</div><div class="sal">Salvaje</div></div><div class="ven">“La Venancia”</div>';
     content.appendChild(heading);
 };
 
